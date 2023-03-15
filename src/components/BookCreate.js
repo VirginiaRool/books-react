9ef4b6f569dd350f2271c790deb33c94c5addf76
VirginiaRool <virginia.aro@gmail.com>
2023-03-15 13:00:49 -0600

    Added classes

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -14,11 +14,12 @@ function BookCreate({ onCreate }) {
   };
 
   return (
-    <div>
+    <div className="book-create">
+      <h3>Add a Book</h3>
       <form onSubmit={handleSubmit}>
         <label>Title:</label>
-        <input value={title} onChange={handleChange} />
-        <button>Create new book</button>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button">Create! 📚</button>
       </form>
     </div>
   );
