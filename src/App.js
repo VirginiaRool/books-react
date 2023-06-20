1ba8f215cb58be95b8b7b8d6c6132a23dc5fbaa9
VirginiaRool <virginia.aro@gmail.com>
2023-06-20 15:27:47 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const deleteBookbyId = (id) => {
-    // Filter can remove elements by index and doesn't modifies the original array, it gives us a new array
+    // Filter can remove elements by index, doesn't modifies the original array, it gives a new array
     const updatedBooks = books.filter((book) => {
       // if returns true, we want to keep the book around.
       // if returns false, we don't want the book anymore.
       return book.id !== id;
       //after create the new array updateBooks, update "books" state
-      setBooks(updatedBooks);
     });
+    setBooks(updatedBooks);
   };
 
   const createBook = (title) => {
