90b124ad59fb1a687331cfc14ec2dd64d49289e1
VirginiaRool <virginia.aro@gmail.com>
2023-06-19 18:11:50 -0600

    Added deleteBookbyId function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookbyId = (id) => {
+    // Filter can remove elements by index and doesn't modifies the original array, it gives us a new array
+    const updatedBooks = books.filter((book) => {
+      // if returns true, we want to keep the book around.
+      // if returns false, we don't want the book anymore.
+      return book.id !== id;
+      //after create the new array "updateBooks", we update the "books" state
+      setBooks(updatedBooks);
+    });
+  };
+
   const createBook = (title) => {
     const updatedBooks = [
       ...books,
@@ -15,7 +26,7 @@ function App() {
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBookbyId} />
       <BookCreate onCreate={createBook} />
     </div>
   );
